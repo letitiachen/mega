5c8ef70deb9639e86a776a0b5278043b57091d52
letitiachen <letitia@megalive.com.tw>
2018-09-27 16:25:50 +0800

    test1

---
+++ test4.java
@@ -0,0 +1,52 @@
+package DataCrawler;
+
+import java.io.OptionalDataException;
+import java.util.Optional;
+
+import DataCrawler.model.ConfigManager;
+import falseapple.GeneralLib.helpers.IOUtil;
+
+public class test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//final Optional<Long> op = testop(1);
+		try {
+			//Optional<Long> op = testop("a");
+			//System.out.println(op);
+			//System.out.println( op.isPresent());
+			//System.out.println( op.isPresent() ? op.get() : false);
+			final String teststr = ConfigManager.getProperties("xxxtestx");
+			if (teststr!=null || "".equals(teststr)) {
+				System.out.println("127");
+			} else {
+				System.out.println(teststr);
+			}
+
+		}finally {
+			
+		}        
+
+	}
+	
+	static public Optional<Long> testop(final String key) {
+        try {
+            return Optional.of(testre(key));
+        } catch (RuntimeException e) {
+            //Logger.fatal(Recorders.system(), LogStamp.SYSTEM_EXCEPTION, IOUtil.traceToString(e));
+        	System.out.println("exception:"+IOUtil.traceToString(e));
+        }
+        System.out.println("2");
+        return Optional.empty();
+    }
+	
+	static public long testre(final String key) {
+        try {
+            return Long.parseLong(key);
+        } finally {
+            System.out.println("testre-finally");
+        }
+    
+    }
+
+}
