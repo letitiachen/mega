cb9095272d2b9a8ea3114e6cf4cfe59ee4dcd587
letitiachen <letitia@megalive.com.tw>
2018-09-27 16:30:47 +0800

    test2

--- test4.java
+++ test4.java
@@ -23,30 +23,35 @@ public class test4 {
 				System.out.println(teststr);
 			}
 
+			final Optional<Map<String, String>> onlineList = getOnlineListInRedis();
+			data.setItem1("線上人數");
+			data.setValue1(String.valueOf(onlineList.isPresent() ? onlineList.get().size() : 0));
+			data.setItem3("待下線人數");
+			data.setValue3(String.valueOf(offlineDatas.size()))
+
 		}finally {
-			
-		}        
+
+		}
 
 	}
-	
+
 	static public Optional<Long> testop(final String key) {
         try {
             return Optional.of(testre(key));
         } catch (RuntimeException e) {
-            //Logger.fatal(Recorders.system(), LogStamp.SYSTEM_EXCEPTION, IOUtil.traceToString(e));
-        	System.out.println("exception:"+IOUtil.traceToString(e));
+            System.out.println("exception:"+IOUtil.traceToString(e));
         }
         System.out.println("2");
         return Optional.empty();
     }
-	
+
 	static public long testre(final String key) {
         try {
             return Long.parseLong(key);
         } finally {
             System.out.println("testre-finally");
         }
-    
+
     }
 
 }
